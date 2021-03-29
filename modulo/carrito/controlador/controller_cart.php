241fa509c0edaebe49db00925712ef04c37a4eaa
AdriRameti <adri7agu@gmail.com>
2021-03-29 19:11:40 +0200

    update cart

--- modulo/carrito/controlador/controller_cart.php
+++ modulo/carrito/controlador/controller_cart.php
@@ -3,5 +3,20 @@ $path=$_SERVER['DOCUMENT_ROOT'] ."/Ejercicios_PHP";
 include ($path  ."/modulo/carrito/modelo/DAOcart.php"); 
 switch ($_GET['op']){
     case 'showCart':
+        try{
+            $daocart = new DAOcart();
+            $rdo = $daocart -> showCart($_GET['user']);
+        }catch (exception(e)){
+            echo json_encode('Error en la busqueda cart');
+        }
+        if (!$rdo){
+            echo json_encode('Error en el resultado cart');
+        }else{
+            $arry = array();
+            foreach ($rdo as $value){
+                array_push($arry,$value);
+            }
+            echo json_encode($arry);
+        }
         break;
 }
--- modulo/carrito/modelo/DAOcart.php
+++ modulo/carrito/modelo/DAOcart.php
@@ -1,3 +1,12 @@
 <?php
 $path=$_SERVER['DOCUMENT_ROOT']  ."/Ejercicios_PHP";
 include ($path ."/modelo/connect.php");
+class DAOcart{
+function showCart(){
+    $sql="";
+    $conexion = connect::con();
+    $res = mysqli_query($conexion, $sql);
+    connect::close($conexion);
+    return $res;
+}
+}
